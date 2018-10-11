5bc33f967c3f3f783f6910758aa8c13082393127
ManivannanE <manivannanelangovan@syncfusion.com>
2018-10-11 17:00:19 +0530

    Legend customization sample committed for all platforms.

---
+++ LegendCustomization/LegendCustomization/LegendCustomization/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LegendCustomization
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+			/* C# code snippet
+            chart.LegendItemCreated += Chart_LegendItemCreated;
+            */
+        }
+
+        private void Chart_LegendItemCreated(object sender, ChartLegendItemCreatedEventArgs e)
+        {
+            Model model = e.LegendItem.DataPoint as Model;
+            e.LegendItem.Label = model.XValue + ": " + model.YValue.ToString();
+            e.LegendItem.LabelStyle = new ChartLegendLabelStyle()
+            {
+                TextColor = model.YValue > 50 ? Color.Green : Color.Red,
+                FontFamily = model.YValue > 50 ? "Times New Roman" : "Arial"
+            };
+        }
+    }
+}
---
+++ LegendCustomization/LegendCustomization/LegendCustomization/Model.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace LegendCustomization
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string xValue;
+        public string XValue
+        {
+            get
+            {
+                return xValue;
+            }
+
+            set
+            {
+                if (value != xValue)
+                {
+                    xValue = value;
+                    OnPropertyChanged("XValue");
+                }
+            }
+        }
+
+        private double yValue;
+        public double YValue
+        {
+            get
+            {
+                return yValue;
+            }
+
+            set
+            {
+                if (value != yValue)
+                {
+                    yValue = value;
+                    OnPropertyChanged("YValue");
+                }
+            }
+        }
+
+        public Model(string xVal, double yVal)
+        {
+            XValue = xVal;
+            YValue = yVal;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
